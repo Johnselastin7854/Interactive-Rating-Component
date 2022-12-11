c8c826925dda149bd01c518504b9feba1a843b6d
selasinmathew <77019516+Johnselastin7854@users.noreply.github.com>
2022-12-11 10:58:44 +0530

    Add files via upload

---
+++ assets/JS/index.js
@@ -0,0 +1,17 @@
+const ratingsContainer=document.querySelector(".ratings__container");
+const thankyouConatiner=document.querySelector(".thankyou__container");
+const submitBtn=document.querySelector(".btn-submit");
+const ratingBtn=document.querySelectorAll(".btn");
+const rating=document.getElementById("rating")
+
+submitBtn.addEventListener("click",()=>{
+    thankyouConatiner.classList.remove("hidden");
+    ratingsContainer.classList.add('hidden');
+})
+
+ratingBtn.forEach(btn=>{
+    btn.addEventListener('click',()=>{
+        rating.innerHTML=btn.innerHTML
+        console.log(btn.innerHTML)
+    })
+})
