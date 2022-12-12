44a4555bca0565d29d11acb4fcef27b719e43d22
selasinmathew <77019516+Johnselastin7854@users.noreply.github.com>
2022-12-12 08:43:10 -0800

    Update index.js

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -4,14 +4,25 @@ const submitBtn=document.querySelector(".btn-submit");
 const ratingBtn=document.querySelectorAll(".btn");
 const rating=document.getElementById("rating")
 
-submitBtn.addEventListener("click",()=>{
-    thankyouConatiner.classList.remove("hidden");
-    ratingsContainer.classList.add('hidden');
-})
+// submitBtn.addEventListener("click",()=>{
+//     thankyouConatiner.classList.remove("hidden");
+//     ratingsContainer.classList.add('hidden');
+// })
+
+// ratingBtn.forEach(btn=>{
+//     btn.addEventListener('click',()=>{
+//         rating.innerHTML=btn.innerHTML
+//         console.log(btn.innerHTML)
+//     })
+// })
 
 ratingBtn.forEach(btn=>{
     btn.addEventListener('click',()=>{
         rating.innerHTML=btn.innerHTML
         console.log(btn.innerHTML)
+        submitBtn.addEventListener("click",()=>{
+            thankyouConatiner.classList.remove("hidden");
+            ratingsContainer.classList.add('hidden');
+        })
     })
-})
+}
